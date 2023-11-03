1ed5730a342797925c42195f1b9a3d3dc818c550
Ryan Mar <69983436+seika5@users.noreply.github.com>
2023-11-02 22:52:07 -0700

    UX update
    ux update, added side scrolling animation
    added some info

---
+++ script.js
@@ -0,0 +1,25 @@
+const pages = document.querySelectorAll(".page");
+const home = document.querySelector(".home");
+const projects = document.querySelector(".projects");
+const contact = document.querySelector(".contact");
+
+slide = (direction) => {
+    let it_go;
+    if (direction == "0") {
+        it_go = 0;
+        home.classList.add("active");
+        projects.classList.remove("active");
+        contact.classList.remove("active");
+    } else if (direction == "1") {
+        it_go = 100;
+        home.classList.remove("active");
+        projects.classList.add("active");
+        contact.classList.remove("active");
+    } else if (direction == "2") {
+        it_go = 200;
+        home.classList.remove("active");
+        projects.classList.remove("active");
+        contact.classList.add("active");
+    }
+    pages.forEach(pages => (pages.style.transform = `translateX(${-it_go}%)`));
+}
