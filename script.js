9e55cdf3b2f6c7d1de6ec7506acaf38497e4d30d
Ryan Mar <69983436+seika5@users.noreply.github.com>
2023-11-03 15:25:11 -0700

    info and animations

--- script.js
+++ script.js
@@ -24,3 +24,42 @@ slide = (direction) => {
     }
     pages.forEach(pages => (pages.style.transform = `translateX(${-it_go}%)`));
 }
+
+jQuery(document).ready(function() {
+
+    var mouseX = 0, mouseY = 0;
+    var xp = 0, yp = 0;
+    var moving = 0;
+
+    setInterval(function() {
+        $(document).mousemove(function (e) {
+            if (mouseX != e.pageX - 1000 && mouseY != e.pageY - 1000) {
+                mouseX = e.pageX - 1000;
+                mouseY = e.pageY - 1000;
+                if (moving < 2950) {
+                    moving += 50;
+                } else {
+                    moving = 3000;
+                }
+            }
+        });
+        if (moving > 0) {
+            moving -= 20;
+        }
+    }, 20);
+
+    setInterval(function () {
+        xp += ((mouseX - xp) / 10);
+        yp += ((mouseY - yp) / 10);
+        $("#circle").css({left: xp + 'px', top: yp + 'px'});
+    }, 20);
+
+    setInterval(function() {
+        if (moving < 2500) {
+            $("#circle").css({opacity: (moving/2500)});
+        } else {
+            $("#circle").css({opacity: 1});
+        }
+    }, 20);
+
+});
