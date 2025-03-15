c2476cce50b966e48a40ef6c49ec3295ed6e924a
Ryan Mar <Phyre205@gmail.com>
2025-03-15 02:13:13 -0700

    analytics2

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -63,6 +63,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <ClientBody>
         {children}
+        <Analytics/>
       </ClientBody>
     </html>
   );
